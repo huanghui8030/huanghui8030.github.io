9f8b807892774205030876567ee644c41344df16
黄卉 <326028030@qq.com>
2017-11-08 16:44:41 +0800

    express框架的用法

--- widget/nodejs/express/app.js
+++ widget/nodejs/express/app.js
@@ -1,21 +1,34 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
+
 var app = express();
 
+app.use(cookieParser());
+var user = {
+    username:'huangh',
+    pwd :'123456'
+}
 app.get('/', function(req, res){
-    console.log(req.session);
-     if (typeof(req.session)!='undefined' && req.session.username) {
-        res.send("Welcome " + req.session.username + "<br>" + "<a href='/logout'>logout</a>");
+    //console.log(req.session);
+    console.log('Cookies-username: ', req.cookies.username);
+    console.log('Cookies-pwd: ', req.cookies.pwd);
+
+     if ( req.cookies.username==user.username 
+        && req.cookies.pwd==user.pwd) {
+        res.send("Welcome " + req.cookies.username + "<br>" + "<a href='/logout'>logout</a>");
     } else {
         res.send("<a href='/login'> Login</a>" + "<br>" + "<a href='/signup'> Sign Up</a>");
     }
     app.locals.title = 'My App';
-    res.send('hello world');
+   
 });
+
+
 app.get('/login', function(req, res){
-   console.log(app.locals.title);
-    res.send('hello world');
+    
+   
 });
 
 
-
-app.listen(3000);
+app.listen(5000);
+console.log('http://127.0.0.1:5000/');
