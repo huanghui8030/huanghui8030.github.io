e6cdf6ae43af54d7a042342dea81f2800881ebf3
黄卉 <326028030@qq.com>
2017-12-15 15:15:47 +0800

    webapck demo

---
+++ widget/webpack/webpack1214/demo03/dist/build-url/0.4b5e315ff6270975dcfe.hot-update.js
@@ -0,0 +1,10 @@
+/*! 压缩单成a.js */
+webpackHotUpdate(0,[
+/* 0 */,
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("var Common = __webpack_require__(2);\n\n__webpack_require__(3);\n__webpack_require__(3);\n\na();\n\nfunction a(){\n    console.log('我是app.js13312323222222');\n    //Styele;\n    console.log('执行a方法');\n\n    Common.commonTest1();\n\n    var a = document.getElementById('root');\n    a.className = 'div2';\n}\n\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zdGF0aWMvanMvdXJsLmpzPzBjNzciXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0FBRUE7QUFDQTs7QUFFQTs7QUFFQTtBQUNBO0FBQ0E7QUFDQTs7QUFFQTs7QUFFQTtBQUNBO0FBQ0EiLCJmaWxlIjoiMS5qcyIsInNvdXJjZXNDb250ZW50IjpbInZhciBDb21tb24gPSByZXF1aXJlKCcuL2NvbW1vbi5qcycpO1xuXG5yZXF1aXJlKCcuLi9jc3MvbGVzcy9hLmxlc3MnKTtcbnJlcXVpcmUoJy4uL2Nzcy9sZXNzL2EubGVzcycpO1xuXG5hKCk7XG5cbmZ1bmN0aW9uIGEoKXtcbiAgICBjb25zb2xlLmxvZygn5oiR5pivYXBwLmpzMTMzMTIzMjMyMjIyMjInKTtcbiAgICAvL1N0eWVsZTtcbiAgICBjb25zb2xlLmxvZygn5omn6KGMYeaWueazlScpO1xuXG4gICAgQ29tbW9uLmNvbW1vblRlc3QxKCk7XG5cbiAgICB2YXIgYSA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290Jyk7XG4gICAgYS5jbGFzc05hbWUgPSAnZGl2Mic7XG59XG5cblxuXG5cblxuLy8vLy8vLy8vLy8vLy8vLy8vXG4vLyBXRUJQQUNLIEZPT1RFUlxuLy8gLi9zdGF0aWMvanMvdXJsLmpzXG4vLyBtb2R1bGUgaWQgPSAxXG4vLyBtb2R1bGUgY2h1bmtzID0gMCJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///1\n");
+
+/***/ })
+])
